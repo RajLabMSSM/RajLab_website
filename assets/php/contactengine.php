c02aca2d37a463db2efd03cec219476d905d70c7
bschilder <brian.schilder@mssm.edu>
2019-01-23 15:31:27 -0500

    Contact form

---
+++ assets/php/contactengine.php
@@ -0,0 +1,38 @@
+<?php
+ 
+$EmailTo = "bschilder@verizon.net";
+$Subject = Trim(stripslashes($_POST['Subject'])); 
+$Name = Trim(stripslashes($_POST['Name']));  
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name; 
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$Email>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=./assets/php/contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
